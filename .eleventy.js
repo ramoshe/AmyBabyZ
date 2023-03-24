134fce6f000a0e70585bf1a08f8cfa64935cd8f6
Rachel Sherard <rsherard@mac.com>
2023-03-24 04:15:38 -0400

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("./src/css/");
+	eleventyConfig.addWatchTarget("./src/css/");
+
+	eleventyConfig.addPassthroughCopy("./src/assets/");
+	eleventyConfig.addWatchTarget("./src/assets/");
+
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        },
+    };
+};
