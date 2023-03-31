1314c11135173a9b6883a893b7d70e99204ac0de
Rachel Sherard <rsherard@mac.com>
2023-03-31 17:51:14 -0400

    get shoutouts popups working

--- src/_includes/components/shoutPop.js
+++ src/_includes/components/shoutPop.js
@@ -1,18 +1,44 @@
 const { html } = require('common-tags');
 
 function shoutPop({title, id, link1, text1, link2, text2, logo, description}) {
-    return html`<div class="popup" id="${id}Pop">
-        <div class="pop-close"><img src="../assets/close.svg /></div>
-		<div class="pop-logo"><img src="../assets/shoutLogos/${logo} /></div>
-		<div class="pop-content">
-			<h1>${title}</h1>
-			<p>${description}</p>
-			<div class="pop-btns">
-				<a href="${link1}" class="btn">${text1}</a>
-				<a href="${link2}" class="btn">${text2}</a>
+    if (link2 == "") {
+        return html`<div class="popup" id="${id}Pop">
+			<div class="pop-close" onclick="popupClose()">
+				<img src="../assets/close.svg" />
 			</div>
-		</div>
-	</div>`;
+			<div id="popcont">
+				<div class="pop-logo">
+					<img src="../assets/shoutLogos/${logo}" />
+				</div>
+				<div class="pop-content">
+					<h1>${title}</h1>
+					<p>${description}</p>
+					<div class="pop-btns">
+						<a href="${link1}" class="btn">${text1}</a>
+					</div>
+				</div>
+			</div>
+		</div>`;
+    } else {
+        return html`<div class="popup" id="${id}Pop">
+            <div class="pop-close" onclick="popupClose()"
+                ><img src="../assets/close.svg"
+            /></div>
+            <div id="popcont">
+                <div class="pop-logo">
+                    <img src="../assets/shoutLogos/${logo}" />
+                </div>
+                <div class="pop-content">
+                    <h1>${title}</h1>
+                    <p>${description}</p>
+                    <div class="pop-btns">
+                        <a href="${link1}" class="btn">${text1}</a>
+                        <a href="${link2}" class="btn">${text2}</a>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    }
 }
 
 module.exports = shoutPop;
